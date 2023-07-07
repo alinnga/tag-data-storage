5b5d2a743db2581d2682b7d0c7b07a9b7a92f28d
alinnga <alina.parfenteva@mail.ru>
2023-07-07 16:19:30 +0300

    check performance

---
+++ src/main/java/com/nppgks/testopctagstorage/CheckPerformance.java
@@ -0,0 +1,40 @@
+package com.nppgks.testopctagstorage;
+
+public class CheckPerformance {
+    public static void main(String[] args) {
+        double[][] arr = new double[1000][1000];
+
+        for(int i = 0; i< 100; i++){
+            long durationByRow = getDurationByRow(arr);
+            long durationByCol = getDurationByCol(arr);
+            if(durationByRow>durationByCol) System.out.println("By row takes more time than by col");
+        }
+    }
+
+    static long getDurationByRow(double[][] arr){
+        long startTime = System.nanoTime();
+
+        for(int i = 0; i< arr.length; i++){
+            for(int j = 0; j < arr.length; j++){
+                arr[i][j] = i+j;
+            }
+        }
+
+        long endTime = System.nanoTime();
+
+        return endTime - startTime;
+    }
+
+    static long getDurationByCol(double[][] arr){
+        long startTime = System.nanoTime();
+
+        for(int i = 0; i< arr.length; i++){
+            for(int j = 0; j < arr.length; j++){
+                arr[j][i] = i+j;
+            }
+        }
+
+        long endTime = System.nanoTime();
+        return endTime - startTime;
+    }
+}
